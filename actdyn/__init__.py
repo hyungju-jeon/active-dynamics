e067023c8e012024adb10f10519665d277b51515
Hyungju Jeon <hyungju.jeon@gmail.com>
2025-06-02 15:50:21 +0100

    clean up

---
+++ actdyn/__init__.py
@@ -0,0 +1,23 @@
+"""
+Active Dynamics: Active Learning for Latent Dynamical System Identification
+"""
+
+from .core.agent import Agent
+from .core.experiment import Experiment
+
+# from .models.vae import VAE
+# from .environment.vectorfield import VectorFieldEnv
+# from .policy.simple_icem import SimpleICem
+# from .metrics.fisher import FisherInformation
+
+# __version__ = "0.1.0"
+
+# __all__ = [
+#     "Agent",
+#     "Experiment",
+#     "VAE",
+#     "EnsembleVAE",
+#     "VectorFieldEnv",
+#     "SimpleICem",
+#     "FisherInformation",
+# ]
