4b48b820cfa483a1eb87cb95c10ab656564db67c
Hyungju Jeon <hyungju.jeon@gmail.com>
2025-09-18 14:14:57 +0100

    added post_run() analysis

--- actdyn/core/experiment.py
+++ actdyn/core/experiment.py
@@ -6,6 +6,8 @@ from actdyn.utils import save_load
 from actdyn.utils.rollout import Rollout, RolloutBuffer
 from actdyn.utils.video import VideoRecorder
 from actdyn.config import ExperimentConfig
+from actdyn.utils.validation_helpers import compute_kstep_r2
+import matplotlib.pyplot as plt
 
 import torch
 import os
@@ -42,7 +44,7 @@ class Experiment:
 
     def run(self, reset=True):
         os.makedirs(self.results_dir, exist_ok=True)
-        for subdir in ["rollouts", "logs"]:
+        for subdir in ["rollouts", "logs", "model"]:
             p = os.path.join(self.results_dir, subdir)
             if os.path.exists(p):
                 shutil.rmtree(p, ignore_errors=True)
@@ -76,20 +78,22 @@ class Experiment:
             self.rollout.add(**transition)
 
             if isinstance(self.training_loss, list):
-                self.writer.add_scalar("elbo", -self.training_loss[0][0], self.env_step)
-                self.writer.add_scalar("log_like", -self.training_loss[0][1], self.env_step)
-                self.writer.add_scalar("kl_divergence", self.training_loss[0][2], self.env_step)
+                self.writer.add_scalar("train/ELBO", -self.training_loss[0][0], self.env_step)
+                self.writer.add_scalar("train/log_like", self.training_loss[0][1], self.env_step)
+                self.writer.add_scalar("train/kl_d", self.training_loss[0][2], self.env_step)
             else:
-                self.writer.add_scalar("elbo", 0, self.env_step)
-                self.writer.add_scalar("log_like", 0, self.env_step)
-                self.writer.add_scalar("kl_divergence", 0, self.env_step)
+                self.writer.add_scalar("train/ELBO", 0, -self.env_step)
+                self.writer.add_scalar("train/log_like", 0, self.env_step)
+                self.writer.add_scalar("train/kl_d", 0, self.env_step)
 
             self.agent.update_policy(transition)
 
             if self.env_step % 10 == 0:
                 if isinstance(self.training_loss, list) and len(self.training_loss) > 0:
-                    elbo_loss = self.training_loss[0][0]
-                    pbar.set_postfix({"ELBO": f"{elbo_loss:.4f}"})
+                    elbo_loss = -self.training_loss[0][0]
+                    pbar.set_postfix(
+                        {"ELBO": f"{elbo_loss:.4f}, beta: {self.agent.model_env.model.beta:.4f}"}
+                    )
                 else:
                     pbar.set_postfix({"ELBO": "N/A"})
                 pbar.update(10)
@@ -124,31 +128,77 @@ class Experiment:
         pbar.close()
         self.rollout.finalize()
         self.writer.close()
+        self.agent.model_env.save_model(os.path.join(self.results_dir, f"model/model_final.pth"))
 
-    def post_run(self):
-        # TODO: Implement k-step prediction and evaluation
-        pass
+    def generate_rollout(self, num_episodes=20, episode_length=1000, rollout_dir=None):
+        num_validate = num_episodes // 3
+        num_train = num_episodes - num_validate
+
+        rb = RolloutBuffer(max_size=num_episodes, device="cpu")
+        pbar = tqdm(total=num_episodes, desc="Validation Episodes")
+        for ep in range(num_episodes):
+            ro = Rollout(device="cpu")
+            state, info = self.agent.env.reset()
+            latent_state = info["latent_state"]
+            for t in range(episode_length):
+                ro.add(obs=state)
+                ro.add(env_state=latent_state)
+                action = self.agent.env.action_space.sample()
+                new_state, reward, _, done, info = self.agent.env.step(action)
+                ro.add(next_obs=new_state)
+                ro.add(action=action)
+                ro.add(next_env_state=info["latent_state"])
+                state = new_state
+                latent_state = info["latent_state"]
+            rb.add(ro)
+            pbar.update(1)
+        pbar.close()
+
+        rb_train = RolloutBuffer(max_size=num_train, device="cpu")
+        rb_train.add(rb[:num_train])
+        rb_validate = RolloutBuffer(max_size=num_validate, device="cpu")
+        rb_validate.add(rb[num_train:])
+
+        if rollout_dir is None:
+            validate_rollout_path = os.path.join(self.results_dir, "validation.pkl")
+            train_rollout_path = os.path.join(self.results_dir, "train.pkl")
+        else:
+            validate_rollout_path = os.path.join(rollout_dir, "validation.pkl")
+            train_rollout_path = os.path.join(rollout_dir, "train.pkl")
+
+        save_load.save_rollout(rb_train, train_rollout_path)
+        save_load.save_rollout(rb_validate, validate_rollout_path)
+        print(f"rollout saved to {validate_rollout_path} and {train_rollout_path}")
+        return rb_train, rb_validate
+
+    def post_run(self, data_dir=None):
+        # Load data if exist
+        if data_dir is None:
+            data_dir = os.path.join(self.results_dir)
+        data_path = os.path.join(data_dir, "validation.pkl")
+
+        if os.path.exists(data_path):
+            rb = save_load.load_rollout(data_path)
+        else:
+            _, rb = self.generate_rollout(num_episodes=50, episode_length=500, rollout_dir=data_dir)
+        #
+        self.writer = SummaryWriter(log_dir=os.path.join(self.results_dir, "logs"))
+
+        # ELBO on validation set
+        validate_elbo(self, rb, self.writer)
+
+        # K-step prediction R2
+        validate_kstep_r2(self, rb, self.writer)
+
+        self.writer.close()
 
     def offline_learning(self):
         self.writer = SummaryWriter(log_dir=os.path.join(self.results_dir, "logs"))
         self.rollout.clear()
         # Check if rollout exists in the results directory
-        rollout_files = [
-            f for f in os.listdir(os.path.join(self.results_dir, "rollouts")) if f.endswith(".pkl")
-        ]
-        if not rollout_files:
-            print("No rollout files found for offline learning.")
-            return
-        rollout_files.sort(key=lambda x: int(x.split("_")[1].split(".")[0]))
-        print("Starting offline learning...")
-
-        # Load and merge rollout
-        self.rollout = Rollout()
-        for rollout_file in rollout_files:
-            rollout_path = os.path.join(self.results_dir, "rollouts", rollout_file)
-            rollout = save_load.load_rollout(rollout_path)
-            self.rollout.add(**rollout._data)
-
+        self.rollout = save_load.load_and_concatenate_rollouts(
+            os.path.join(self.results_dir, "rollouts")
+        )
         offline_cfg = self.cfg.training.get_offline_optim_cfg()
 
         # Perform offline learning
@@ -156,12 +206,41 @@ class Experiment:
 
         elbo_list, loglike_list, kl_list = [], [], []
         for t in self.training_loss:
-            elbo_list.append(-float(t[0]))
-            loglike_list.append(-float(t[1]))
+            elbo_list.append(float(-t[0]))
+            loglike_list.append(float(t[1]))
             kl_list.append(float(t[2]))
 
         for i, (e, l, k) in enumerate(zip(elbo_list, loglike_list, kl_list), start=1):
-            self.writer.add_scalar("offline/elbo", e, i)
-            self.writer.add_scalar("offline/log_like", l, i)
-            self.writer.add_scalar("offline/kl_divergence", k, i)
+            self.writer.add_scalar("offline/train/ELBO", e, i)
+            self.writer.add_scalar("offline/train/log_like", l, i)
+            self.writer.add_scalar("offline/train/kl_d", k, i)
         self.writer.close()
+
+
+def validate_elbo(experiment: Experiment, rb: Rollout | RolloutBuffer, writer: SummaryWriter):
+    B, T, D = rb["obs"].shape
+    loss, log_like, kl_d = experiment.agent.model_env.model.compute_elbo(
+        y=rb["next_obs"],
+        u=rb["action"],
+        idx=None,
+        n_samples=16,
+        beta=1,
+        k_steps=1,
+    )
+    writer.add_scalar("validation/ELBO", -loss.item() / T, 0)
+    writer.add_scalar("validation/log_like", log_like.item() / T, 0)
+    writer.add_scalar("validation/kl_d", kl_d.item() / T, 0)
+
+
+def validate_kstep_r2(experiment: Experiment, rb: Rollout | RolloutBuffer, writer: SummaryWriter):
+    r2_fig_path = os.path.join(experiment.results_dir, "validation_kstep.png")
+    r2m, _ = compute_kstep_r2(
+        model=experiment.agent.model_env.model,
+        rollout=rb,
+        k_max=10,
+        n_samples=50,
+        fig_path=r2_fig_path,
+    )
+    for i in range(r2m.shape[1]):
+        for k in range(r2m.shape[0]):
+            writer.add_scalar(f"validation/kstep/r2_mean_{i}", r2m[k, i], k + 1)
