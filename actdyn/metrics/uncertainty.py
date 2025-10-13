012501d2c777a9f43795d2ca198512bd1a913c4e
Hyungju Jeon <hyungju.jeon@gmail.com>
2025-10-13 11:28:16 +0100

    Random Network Distillation

--- actdyn/metrics/uncertainty.py
+++ actdyn/metrics/uncertainty.py
@@ -34,3 +34,38 @@ class EnsembleDisagreement(BaseMetric):
         self.metric = uncertainty.mean(dim=-1).sum(dim=-1).unsqueeze(-1)
 
         return self.metric
+
+
+class RandomNetworkDistillation(BaseMetric):
+    """Metric to compute the Random Network Distillation (RND) uncertainty."""
+
+    def __init__(
+        self,
+        target_network: torch.nn.Module,
+        predictor_network: torch.nn.Module,
+        compute_type="sum",
+        device: str = "cpu",
+        **kwargs
+    ):
+        super().__init__(compute_type, device)
+        self.target_network = target_network.to(device).eval()
+        self.predictor_network = predictor_network.to(device).train()
+
+    def compute_uncertainty(self, x):
+        """Compute uncertainty using RND."""
+        with torch.no_grad():
+            target_features = self.target_network(x)
+        pred_features = self.predictor_network(x)
+        uncertainty = ((pred_features - target_features) ** 2).mean(dim=-1)
+        return uncertainty
+
+    def compute(self, rollout: Union[Rollout, RolloutBuffer, Dict]) -> torch.Tensor:
+        """Compute the RND uncertainty metric."""
+        uncertainty = self.compute_uncertainty(rollout["model_state"])
+        self.metric = uncertainty.unsqueeze(-1)
+
+        return self.metric
+
+
+if __name__ == "__main__":
+    pass
