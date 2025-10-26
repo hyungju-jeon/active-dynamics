a80e89e2910721938afeb5527ab365541eca3662
Hyungju Jeon <hyungju.jeon@gmail.com>
2025-10-26 17:34:48 +0000

    adding animation

--- actdyn/core/experiment.py
+++ actdyn/core/experiment.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from actdyn.utils import save_load
 from actdyn.utils.rollout import Rollout, RolloutBuffer
 from actdyn.utils.video import VideoRecorder
-from actdyn.utils.helper import format_list
+from actdyn.utils.helper import format_list, to_np
 from actdyn.config import ExperimentConfig
 from actdyn.utils.validation import compute_kstep_r2
 import matplotlib.pyplot as plt
@@ -14,6 +14,8 @@ import torch
 import os
 import shutil
 import copy
+
+from actdyn.utils.visualize import plot_vector_field
 
 
 class Experiment:
@@ -25,13 +27,46 @@ class Experiment:
         self.rollout = Rollout(device="cpu")
         self.writer = None
         self.video_recorder = None
-        self.training_loss = [0.0, 0.0, 0.0]
+        self.training_info = {}
         self.results_dir = os.path.join(os.path.dirname(__file__), config.results_dir)
         if resume:
             ## TODO : Implement resume functionality
             print("Resuming from previous experiment state...")
 
             # self.agent.model.load(self.cfg.logging.model_path)
+
+    def init_experiment(self, reset=True):
+        os.makedirs(self.results_dir, exist_ok=True)
+        for subdir in ["rollouts", "logs", "model"]:
+            p = os.path.join(self.results_dir, subdir)
+            if os.path.exists(p):
+                shutil.rmtree(p, ignore_errors=True) if reset else None
+            os.makedirs(p, exist_ok=True)
+
+        self.writer = SummaryWriter(log_dir=os.path.join(self.results_dir, "logs"))
+
+        # Initialize environment
+        if reset:
+            self.agent.reset(seed=int(self.cfg.seed))
+            self.env_step = 0
+            self.rollout.clear()
+        else:
+            print("Continuing from previous step:", self.env_step)
+            self.rollout.clear()
+
+    # TODO: Add video recording functionality
+    def _setup_video_recording(self, video_path, fps=30):
+        if video_path:
+            self.video_recorder = VideoRecorder(self.agent.env, video_path, fps=fps)
+            self.video_recorder.capture_frame()
+        else:
+            self.video_recorder = None
+
+    # TODO: Add video recording functionality
+    def _stop_video_recording(self):
+        if self.video_recorder:
+            self.video_recorder.save()
+            self.video_recorder = None
 
     def generate_rollout(self, num_episodes=20, episode_length=1000, rollout_dir=None):
         num_validate = num_episodes // 3
@@ -75,29 +110,23 @@ class Experiment:
         print(f"rollout saved to {validate_rollout_path} and {train_rollout_path}")
         return rb_train, rb_validate
 
-    # TODO: Add video recording functionality
-    def _setup_video_recording(self, video_path):
-        if video_path:
-            self.video_recorder = VideoRecorder(self.agent.env, video_path)
-            self.video_recorder.capture_frame()
-        else:
-            self.video_recorder = None
+    def update_writer(self, info: dict, prefix=""):
+        # Update logs
+        for key, value in info.items():
+            # if there is multiple values (e.g., list or tensor), log them using add_scalars
+            if isinstance(value, (list, torch.Tensor)) and len(value) > 1:
+                if isinstance(value, torch.Tensor):
+                    value = value.detach().cpu().numpy().tolist()
+                scalar_dict = {f"{i}": v for i, v in enumerate(value)}
+                self.writer.add_scalars(prefix + key, scalar_dict, self.env_step)
+            else:
+                self.writer.add_scalar(prefix + key, value, self.env_step)
 
-    # TODO: Add video recording functionality
-    def _stop_video_recording(self):
-        if self.video_recorder:
-            self.video_recorder.save()
-            self.video_recorder = None
-
-    def update_pbar(self, pbar, interval=100, postfix={}, vars=[]):
-        if "ELBO" in vars:
-            elbo_loss = -self.training_loss[0][0]
-            postfix.update({"ELBO": f"{elbo_loss:.4f}"})
-        elif "beta" in vars:
-            postfix.update({"beta": f"{self.agent.model.model.beta:.4f}"})
-
+    def update_pbar(self, pbar: tqdm, interval: int = 100, postfix: dict = {}):
         if self.env_step % interval == 0 and self.env_step > 0:
-            pbar.set_postfix(postfix)
+            pbar.set_postfix(
+                {k: f"{format_list(v)}" for k, v in self.training_info.items()} | postfix
+            )
             pbar.update(interval)
 
     @property
@@ -108,38 +137,24 @@ class Experiment:
         )
 
     @property
-    def is_logging_step(self):
-        return self.env_step % self.cfg.logging.plot_every == 0
+    def is_save_step(self):
+        return self.env_step % self.cfg.logging.save_every == 0
 
     def run(self, reset=True):
-        os.makedirs(self.results_dir, exist_ok=True)
-        for subdir in ["rollouts", "logs", "model"]:
-            p = os.path.join(self.results_dir, subdir)
-            if os.path.exists(p):
-                shutil.rmtree(p, ignore_errors=True) if reset else None
-            os.makedirs(p, exist_ok=True)
-
-        self.writer = SummaryWriter(log_dir=os.path.join(self.results_dir, "logs"))
         train_cfg = self.cfg.training
-        # Initialize environment
-        if reset:
-            self.agent.reset(seed=int(self.cfg.seed))
-            self.env_step = 0
-            self.rollout.clear()
-        else:
-            print("Continuing from previous step:", self.env_step)
-            self.rollout.clear()
 
+        # Initialize environment
+        self.init_experiment(reset=reset)
+        self._setup_video_recording(os.path.join(self.results_dir, "dynamics.mp4"))
         # Setup progress bar
         pbar = tqdm(total=train_cfg.total_steps - self.env_step, desc="Training")
         while self.env_step < train_cfg.total_steps:
             self.env_step += 1
-
-            with torch.no_grad():
-                # 1. Plan
-                action = self.agent.plan()
-                # 2. Execute
-                transition, done = self.agent.step(action)
+            print("hi")
+            # 1. Plan
+            action = self.agent.plan()
+            # 2. Execute
+            transition, done = self.agent.step(action)
 
             # Append transition to rollout
             self.rollout.add(**transition)
@@ -148,27 +163,38 @@ class Experiment:
             self.agent.update_policy(transition)
 
             # Update logs
-            self.writer.add_scalar("ELBO", -self.training_loss[0], self.env_step)
-            self.writer.add_scalar("log_like", self.training_loss[1], self.env_step)
-            self.writer.add_scalar("kl_d", self.training_loss[2], self.env_step)
-
-            self.update_pbar(pbar, vars=["ELBO", "beta"])
+            self.update_writer(self.training_info)
+            self.update_pbar(pbar)
 
             # Train model periodically
             if self.is_training_step:
-                sampling_ratio = self.agent.model.model.dynamics.dt / self.agent.env.dt
-                self.training_loss = self.agent.train_model(
+                sampling_ratio = self.agent.model.dynamics.dt / self.agent.env.dt
+                self.training_info = self.agent.train_model(
                     **train_cfg.get_optim_cfg(), sampling_ratio=sampling_ratio
-                )[0]
+                )
 
             # Periodic rollout saving for crash recovery and memory management
-            if self.is_logging_step:
+            if self.is_save_step:
                 save_load.save_rollout(
                     self.rollout,
                     os.path.join(self.results_dir, f"rollouts/rollout_{self.env_step}.pkl"),
                 )
                 if self.env_step < train_cfg.total_steps:
                     self.rollout.clear()
+                # for param in self.agent.policy.metric.metrics[0].predictor_network.parameters():
+                #     param.data += 1e-3 * torch.randn_like(param.data)
+
+            if self.env_step % self.cfg.logging.plot_every == 0 and self.env_step > 0:
+                # Save intermediate model
+                z = self.agent.recent["env_state"]
+                z_hat = self.agent.recent["model_state"]
+
+                plot_vector_field(self.agent.model.dynamics, x_range=5)
+                # plot_vector_field(duffing_env.dynamics, x_range=3)
+                plt.plot(to_np(z[0])[:, 0], to_np(z[0])[:, 1], alpha=0.7, label="true")
+                plt.plot(to_np(z_hat[0])[:, 0], to_np(z_hat[0])[:, 1], alpha=0.7, label="model")
+                plt.legend()
+                plt.show()
 
             # Clean up tensors to prevent memory accumulation
             if "cuda" in str(self.agent.device):
@@ -179,7 +205,7 @@ class Experiment:
                 break
         pbar.close()
         self.rollout.finalize()
-        self.agent.model.save_model(os.path.join(self.results_dir, f"model/model_final.pth"))
+        self.agent.model.save(os.path.join(self.results_dir, f"model/model_final.pth"))
 
     def post_run(self, data_dir=None):
         # Load data if exist
@@ -280,39 +306,25 @@ class Experiment:
 
 
 class MetaEmbeddingExperiment(Experiment):
-    def __init__(self, agent: Agent, config: ExperimentConfig, resume=False):
-        super().__init__(agent, config, resume=resume)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def run(self, reset=True):
-        os.makedirs(self.results_dir, exist_ok=True)
-        for subdir in ["rollouts", "logs", "model"]:
-            p = os.path.join(self.results_dir, subdir)
-            if os.path.exists(p):
-                shutil.rmtree(p, ignore_errors=True) if reset else None
-            os.makedirs(p, exist_ok=True)
-
-        self.writer = SummaryWriter(log_dir=os.path.join(self.results_dir, "logs"))
         train_cfg = self.cfg.training
+        self._setup_video_recording(os.path.join(self.results_dir, "dynamics.mp4"), fps=60)
         # Initialize environment
-        if reset:
-            self.agent.reset(seed=int(self.cfg.seed))
-            self.env_step = 0
-            self.rollout.clear()
-        else:
-            print("Continuing from previous step:", self.env_step)
-            self.rollout.clear()
+        self.init_experiment(reset=reset)
 
         # Setup progress bar
-        pbar = tqdm(total=train_cfg.total_steps - self.env_step, desc="Training")
+        self.pbar = tqdm(total=train_cfg.total_steps - self.env_step, desc="Training")
         while self.env_step < train_cfg.total_steps:
             self.env_step += 1
 
-            with torch.no_grad():
-                # 1. Plan
-                action = self.agent.plan()
-                # 2. Execute
-                transition, done = self.agent.step(action)
-                e_bel = self.agent.model.embedding
+            # 1. Plan
+            action = self.agent.plan()
+            # 2. Execute
+            transition, done = self.agent.step(action)
+            e_bel = self.agent.model.embedding.reshape(-1)
 
             # Append transition to rollout
             self.rollout.add(**transition)
@@ -321,29 +333,46 @@ class MetaEmbeddingExperiment(Experiment):
             self.agent.update_policy(transition)
 
             # Update logs
-            self.writer.add_scalar("log_like", self.training_loss[0], self.env_step)
-            self.writer.add_scalar("kl_d", self.training_loss[2], self.env_step)
-            self.writer.add_scalar("e", e_bel, self.env_step)
-
-            self.update_pbar(
-                pbar,
-                postfix={"log_like": f"{self.training_loss[0]:.3f}", "e": format_list(e_bel)},
+            self.training_info["e"] = e_bel
+            self.update_writer(self.training_info)
+            self.writer.add_scalars(
+                "e",
+                {
+                    "true_0": self.agent.env.env.dyn_param[0],
+                    "true_1": self.agent.env.env.dyn_param[1],
+                },
+                self.env_step,
             )
+            self.update_pbar(self.pbar)
 
             # Train model periodically
             if self.is_training_step:
-                sampling_ratio = self.agent.model.model.dynamics.dt / self.agent.env.dt
-                self.training_loss = self.agent.train_model(
-                    **train_cfg.get_optim_cfg(), sampling_ratio=sampling_ratio
-                )[0]
+                sampling_ratio = self.agent.model.dynamics.dt / self.agent.env.dt
+                # self.training_info = self.agent.train_model(
+                #     **train_cfg.get_optim_cfg(), sampling_ratio=sampling_ratio
+                # )
 
             # Periodic rollout saving for crash recovery and memory management
-            if self.is_logging_step:
+            if self.is_save_step:
                 save_load.save_rollout(
                     self.rollout,
                     os.path.join(self.results_dir, f"rollouts/rollout_{self.env_step}.pkl"),
                 )
                 self.rollout.clear()
+
+            if self.env_step % self.cfg.logging.plot_every == 0 and self.env_step > 0:
+                # Save intermediate model
+                fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+                plot_vector_field(
+                    self.agent.model.dynamics, title="Learned Dynnimics", ax=ax, x_range=5
+                )
+                self.video_recorder.capture_frame(fig=fig)
+                plt.close(fig)
+                # plot_vector_field(duffing_env.dynamics, x_range=3)
+                # plt.plot(to_np(z[0])[:, 0], to_np(z[0])[:, 1], alpha=0.7, label="true")
+                # plt.plot(to_np(z_hat[0])[:, 0], to_np(z_hat[0])[:, 1], alpha=0.7, label="model")
+                # plt.legend()
+                # plt.show()
 
             # Clean up tensors to prevent memory accumulation
             if "cuda" in str(self.agent.device):
@@ -353,5 +382,13 @@ class MetaEmbeddingExperiment(Experiment):
             if done:
                 break
 
-        pbar.close()
-        self.agent.model.save_model(os.path.join(self.results_dir, f"model/model_final.pth"))
+        self.pbar.close()
+        self.agent.model.save(os.path.join(self.results_dir, f"model/model_final.pth"))
+        self.video_recorder.close()
+
+    # When closed or deleted
+    def __del__(self):
+        if self.writer:
+            self.writer.close()
+        if hasattr(self, "pbar") and self.pbar is not None:
+            self.pbar.close()
