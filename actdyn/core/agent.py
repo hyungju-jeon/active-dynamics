8554d9506948516f65da6fab93afb695d39931cc
Hyungju Jeon <hyungju.jeon@gmail.com>
2025-11-04 17:07:50 +0000

    Clean up Agent

--- actdyn/core/agent.py
+++ actdyn/core/agent.py
@@ -1,6 +1,9 @@
-from typing import Dict
+from typing import Dict, Tuple
 import torch
-from actdyn.utils.rollout import RecentRollout
+from actdyn.environment.vectorfield import VectorFieldEnv
+from actdyn.metrics.uncertainty import RandomNetworkDistillation
+from actdyn.models.decoder import GaussianNoise, PoissonNoise
+from actdyn.utils.rollout import RecentRollout, RolloutBuffer
 from actdyn.environment.env_wrapper import GymObservationWrapper
 from actdyn.models.base import BaseModel
 from actdyn.policy.base import BasePolicy
@@ -8,26 +11,17 @@ from actdyn.policy.mpc import BaseMPC
 from actdyn.metrics.base import CompositeMetric
 from actdyn.metrics.information import FisherInformationMetric
 from einops import rearrange
-
-Belief = Dict[str, torch.Tensor]
+from actdyn.utils import Transition, Belief
 
 
 class Agent:
-    """Agent class for active learning in dynamical systems.
-
-    Args:
-        env: The environment to interact with
-        model_env: The internal model(VAE) for state estimation
-        policy: The policy for action selection
-        action_encoder: The learnable action function g(.)
-        device (str, optional): Device to run on. Defaults to "cuda".
-    """
+    """Agent class for active learning in dynamical systems."""
 
     def __init__(
         self,
         env: GymObservationWrapper,
         model: BaseModel,
-        policy: BasePolicy | BaseMPC,
+        policy: BasePolicy,
         buffer_length: int = 20,
         device="cuda",
     ):
@@ -39,20 +33,16 @@ class Agent:
 
         # Buffers on GPU for training
         self.buffer_length = buffer_length
-        self.recent = RecentRollout(max_len=self.buffer_length, device=str(self.device))
+        self.recent = RecentRollout(max_len=self.buffer_length, device=device)
 
         # State tracking
         self._observation = None  # Current observation from the environment
         self._env_state = None  # Current environment state
         self._model_state = None  # Agent's internal state estimate
 
-    def reset(self, seed: int | None = None):
-        """Reset the agent and environment.
-
-        Returns:
-            torch.Tensor: Initial observation
-        """
-        # Get initial observation from environment
+    def reset(self, seed: int | None = None) -> torch.Tensor:
+        """Reset the agent and environment."""
+        # Reset environment and get initial observation
         with torch.no_grad():
             obs, info = self.env.reset(seed=seed)
             # Seed action space as well for deterministic sampling
@@ -62,31 +52,96 @@ class Agent:
             except Exception:
                 pass
             self._observation = obs
+            # Reset model
             _, model_info = self.model.reset(self._observation)
 
         # Initialize internal states
         self._env_state = info["latent_state"]
         self._model_state = model_info["latent_state"]
 
+        # Reset recent buffer
         self.recent = RecentRollout(max_len=self.buffer_length, device=str(self.device))
 
         return self._observation
 
-    def step(self, action):
-        """Take a step in the environment.
+    def step(self, action: torch.Tensor | None = None) -> Tuple[Transition, bool]:
+        """Take a step in the environment."""
 
-        Args:
-            action: Action to take (raw action, will be encoded by action_encoder)
+        # Step environment with the encoded action
+        obs, reward, terminated, truncated, env_info = self.env.step(action)
+        done = terminated or truncated
 
-        Returns:
-            Tuple containing:
-                - obs: Next observation
-                - reward: Reward received
-                - done: Whether episode is done
-                - info: Additional information
-        """
-        # Update the policy
-        # self.update_policy(self.recent)
+        # Store transition for training
+        env_transition = {
+            "obs": self._observation,  # Observation  y_t
+            "next_obs": obs,  # New Observation y_{t+1}
+            "action": action,  # Action a_t
+            "env_action": env_info["env_action"],  # Env encoded action g(a_t)
+            "reward": reward,  # Env Reward
+            "env_state": self._env_state,  # Environment state z_t
+            "next_env_state": env_info["latent_state"],  # Next environment state z_{t+1}
+            "model_state": self._model_state,
+        }
+        self.recent.add(**env_transition)
+
+        # Update and take a step in the model environment with the action taken
+        model_info = self.model.update(self.recent)
+        model_transition = {
+            "model_action": model_info["env_action"],  # Model encoded action g'(a_t)
+            "next_model_state": model_info["latent_state"],  # Next belief state z'_{t+1}
+        }
+        self.recent.add(**model_transition)
+        transition = {**env_transition, **model_transition}
+
+        # Update the policy state based on the recent transition
+        self.update_policy(self.recent)
+
+        # Update observation and environment/model state
+        self._observation = obs
+        self._env_state = env_info["latent_state"]
+        self._model_state = self.model._state
+
+        return transition, done
+
+    def plan(self) -> torch.Tensor:
+        """Plan next action using the policy."""
+        # Use policy to plan and get action
+        action = self.policy(self._model_state)
+        return action
+
+    def update_policy(self, transition: Transition) -> None:
+        """Update the policy based on the latest transition."""
+        # Update policy if necessary (only on MPC for now)
+        if isinstance(self.policy, BaseMPC):
+            for metric in self.policy.metric.metric_list:
+                metric.update(transition)
+
+    def train_model(self, sampling_ratio: int = 1, **kwargs) -> Dict[str, float | torch.Tensor]:
+        """Train the model using recent transitions."""
+        data = self.recent.copy()
+        data.downsample(n=int(sampling_ratio))
+
+        train_info = self.model.train_model(data, batch_size=len(data), **kwargs)
+        return train_info
+
+
+class AsyncAgent(Agent):
+    """Agent with asynchronous filtering and update frequency for Poisson observation"""
+
+    def __init__(
+        self,
+        env: GymObservationWrapper,
+        model: BaseModel,
+        policy: BasePolicy,
+        buffer_length: int = 20,
+        device="cuda",
+    ):
+        super().__init__(env, model, policy, buffer_length, device)
+        if not isinstance(self.model.decoder.noise, PoissonNoise):
+            print("Warning: AsyncAgent is designed for models with Poisson observation noise.")
+
+    def step(self, action: torch.Tensor | None = None) -> Tuple[Transition, bool]:
+        """Take a step in the environment."""
 
         # Step environment with the encoded action
         obs, reward, terminated, truncated, env_info = self.env.step(action)
@@ -104,8 +159,10 @@ class Agent:
         }
         self.recent.add(**env_transition)
 
-        # Update model with the action taken
-        model_info = self.model.update(self.recent["next_obs"], self.recent["action"])
+        # Prediction update in the model with the new observation and action
+        model_info = self.model.predict_state(action)
+        model_info["latent_state"] = self.model.update_prediction(self.recent)
+
         model_transition = {
             "model_action": model_info["env_action"],  # Model encoded action g'(a_t)
             "model_state": self._model_state,  # Current belief state z'_t
@@ -113,7 +170,14 @@ class Agent:
         }
         self.recent.add(**model_transition)
 
+        # Measurement update if enough data is collected
+        if len(self.recent) >= self.buffer_length:
+            self.model.update_posterior(self.recent)
+
         transition = {**env_transition, **model_transition}
+
+        # Update the policy state based on the recent transition
+        self.update_policy(self.recent)
 
         # Update observation and environment/model state
         self._observation = obs
@@ -121,32 +185,3 @@ class Agent:
         self._model_state = self.model._state
 
         return transition, done
-
-    def plan(self):
-        """Plan next action using the policy.
-
-        Returns:
-            torch.Tensor: Selected action
-        """
-        # Use policy to plan and get action
-        action = self.policy(self._model_state)
-        return action
-
-    def update_policy(self, transition):
-        """Update the policy based on the latest transition."""
-        # Update policy if necessary
-        if isinstance(self.policy, BaseMPC):
-            if isinstance(self.policy.metric, CompositeMetric):
-                for metric in self.policy.metric.metrics:
-                    if isinstance(metric, FisherInformationMetric):
-                        metric.update_fim(transition)
-            if isinstance(self.policy.metric, FisherInformationMetric):
-                self.policy.metric.update_fim(transition)
-
-    def train_model(self, sampling_ratio=1, **kwargs) -> Dict[str, float]:
-        """Train the model using recent transitions."""
-        data = self.recent.copy()
-        data.downsample(n=int(sampling_ratio))
-
-        train_info = self.model.train_model(data, batch_size=len(data), **kwargs)
-        return train_info
